78c39b1da38b9220881b13603d349a7ae855c09d
박유진 <yujin3717@siwonschool.com>
2021-12-06 13:51:23 +0900

    21.12.06_ 이미지 변경 및 배경 작업

--- js/daily.js
+++ js/daily.js
@@ -1,44 +1,50 @@
 (function () {
 
-    const stageElem = document.querySelector( '.stage' );
-    const houseElem = document.querySelector( '.house' );
-    const barElem = document.querySelector( '.progress_bar');
-    const mousePos = { x: 0, y: 0};
-    let maxScrollValue;
+	const stageElem = document.querySelector( '.stage' );
+	const houseElem = document.querySelector( '.house' );
+	const barElem = document.querySelector( '.progress_bar');
+	const mousePos = { x: 0, y: 0};
+	let maxScrollValue;
 
-    function resizehandler() {
-        maxScrollValue =  document.body.offsetHeight - window.innerHeight;
-    }
+	function resizehandler() {
+		maxScrollValue =  document.body.offsetHeight - window.innerHeight;
+	}
 
-    window.addEventListener( 'scroll', function() {
-        const scrollPer = pageYOffset / maxScrollValue;
-        const zMove =  scrollPer * 1000 - 490;
-        houseElem.style.transform = 'translateZ(' + zMove + 'vw)';
+	window.addEventListener( 'scroll', function() {
+		const scrollPer = pageYOffset / maxScrollValue;
+		const zMove =  scrollPer * 1000 - 490;
+		houseElem.style.transform = 'translateZ(' + zMove + 'vw)';
 
-        // progressBar
-        barElem.style.width =  scrollPer * 100 + '%';
-    });
-    
-    // mouse hover시 moving
-    window.addEventListener('mousemove', function (e) {
-        mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
-        mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+		// progressBar
+		barElem.style.width =  scrollPer * 100 + '%';
+	});
 
-        stageElem.style.transform = 'rotateX(' + (mousePos.y * 5) + '0deg) rotateY(' + (mousePos.x * 5) + 'deg)';
-    });
-    
-    window.addEventListener('resize', resizehandler);
-    resizehandler();
+	// mouse hover시 moving
+	window.addEventListener('mousemove', function (e) {
+		mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+		mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+
+		stageElem.style.transform = 'rotateX(' + (mousePos.y * 5) + '0deg) rotateY(' + (mousePos.x * 5) + 'deg)';
+	});
+
+	window.addEventListener('resize', resizehandler);
+	resizehandler();
+
 })();
 
 $(document).ready(function (){
 
-    const sl_book = new Swiper('.sl_book', {
-        centeredSlides: true,
-        slidesPerView: 4,
-        spaceBetween: 10,
-        loop: true,
-    });
+	const sl_book = new Swiper('.sl_book', {
+		centeredSlides: true,
+		slidesPerView: 4,
+		spaceBetween: 10,
+		loop: true,
+		touchRatio: 0,
+		navigation: {
+			prevEl: '.navigation .prev',
+			nextEl: '.navigation .next'
+		},
+	});
 
 });
 
