3dfe31e46126cca2765b74d8450b5d5825e0cbcc
yujin000915 <yujin3717@naver.com>
2023-05-31 15:58:28 +0900

    특별한 이유 단락 제거

--- js/toeicpack.js
+++ js/toeicpack.js
@@ -132,18 +132,6 @@ ScrollTrigger.create({
 });
 
 ScrollTrigger.create({
-	trigger: '.sec_workbook_tit',
-	scroller: ".scroller",
-	start: "top",
-	onEnter: () => {
-		$('.workbook_list').addClass('on');
-	},
-	onLeaveBack: () => {
-		$('.workbook_list').removeClass('on');
-	},
-});
-
-ScrollTrigger.create({
 	trigger: '.sec_curri',
 	scroller: ".scroller",
 	start: "top",
