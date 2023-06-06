b45e0149051f865b84abf665eb15c504101ef131
yujin000915 <qkrdbwlsdl@gmail.com>
2023-06-06 17:07:01 +0900

    마지막 단락 로고 추가

--- js/toeicpack.js
+++ js/toeicpack.js
@@ -212,7 +212,6 @@ ScrollTrigger.create({
 	},
 });
 
-
 ScrollTrigger.create({
 	trigger: '.memopad_tit',
 	scroller: ".scroller",
@@ -225,8 +224,35 @@ ScrollTrigger.create({
 	},
 });
 
+ScrollTrigger.create({
+	trigger: '.logo',
+	scroller: ".scroller",
+	start: "top-=20%",
+	onEnter: () => {
+		setTextAnimation(0, 5, 2,'ease','#20245a',true);
+
+        setInterval(function() {
+            $('.hide_text').hide();
+            $('#lab_l').css('transform', 'translateX(34%)');
+            $('#lab_a').css('transform', 'translateX(20%)');
+            $('#lab_b').css('transform', 'translateX(10%)');
+        }, 3500);
+	},
+});
 
 // svg
-const pathLen = $('path').length;
+function setTextAnimation(delay, duration, strokeWidth, timingFunction, strokeColor, repeat) {
+    let paths = document.querySelectorAll("path");
+    let mode=repeat?'forwards':'infinite'
+    for (let i = 0; i < paths.length; i++) {
+        const path = paths[i];
+        const length = path.getTotalLength();
+        path.style["stroke-dashoffset"] = `${length}px`;
+        path.style["stroke-dasharray"] = `${length}px`;
+        path.style["stroke-width"] = `${strokeWidth}px`;
+        path.style["stroke"] = `${strokeColor}`;
+        path.style["animation"] = `${duration}s svg-text-anim ${mode} ${timingFunction}`;
+        path.style["animation-delay"] = `${i * delay}s`;
+    }
+}
 
-console.log(pathLen);
